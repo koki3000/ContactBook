84f448a8d9311db4e600efd5e19c215942b788f4
Konrad P <kokiziom@gmail.com>
2023-04-07 18:20:21 +0200

    Default null Date field in Contact

--- book/models.py
+++ book/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Contact(models.Model):
     name = models.TextField(null=True, blank=True)
     number = models.IntegerField()
-    birthday = models.DateField
+    birthday = models.DateField(null=True, blank=True)
     info = models.TextField(null=True, blank=True)
 
     def __str__(self):
