5e432f6229a5c5d29c431ba3071f225a0f69a51a
Konrad P <kokiziom@gmail.com>
2023-04-07 17:48:06 +0200

    Added Model Contact

--- book/models.py
+++ book/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.TextField(null=True, blank=True)
+    number = models.IntegerField()
+    birthday = models.DateField
+    info = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name[0:25]
