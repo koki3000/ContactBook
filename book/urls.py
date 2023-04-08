4fc144ca10f4ff0bc5ffb0d174d450ca9669f7a0
Konrad P <kokiziom@gmail.com>
2023-04-08 23:38:31 +0200

    Added base.html and simple GET view

--- book/urls.py
+++ book/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+from . models import Contact
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.HomePageView.as_view(), name="home"),
 ]
--- book/views.py
+++ book/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import HttpResponse, render
+from django.views.generic.list import ListView
+from . models import Contact
 
 # Create your views here.
 
-def home(request):
+class HomePageView(ListView):
 
-    return HttpResponse('Page')
+    model = Contact
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = Contact.objects.all()
+        return context
