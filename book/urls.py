d8b16135cdc0bb505a802db5ac409484bf419fbf
Konrad P <kokiziom@gmail.com>
2023-04-11 21:29:57 +0200

    Added update and delete contact

--- book/urls.py
+++ book/urls.py
@@ -1,9 +1,32 @@
 from django.urls import path, reverse_lazy
 from . import views
 from . models import Contact
+from django.views.generic import CreateView, UpdateView, DeleteView
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name="home"),
-    path('contact/create/', views.CreateContact.as_view(), name="create-contact"),
-    path('contact/<slug:pk>/', views.ContactView.as_view(), name="contact"),
+    path('contact/<int:pk>/', views.ContactView.as_view(), name="contact"),
+    path('contact/create/',
+         CreateView.as_view(
+            model=Contact,
+            fields='__all__',
+            success_url=reverse_lazy("home"),
+            template_name='book/contact_create_form.html'
+         ),
+         name='contact-create'),
+    path('contact/<int:pk>/update/',
+         UpdateView.as_view(
+            model=Contact,
+            fields='__all__',
+            success_url=reverse_lazy("home"),
+            template_name='book/contact_update_form.html'
+         ),
+         name='contact-update'),
+    path('contact/<int:pk>/delete/',
+         DeleteView.as_view(
+            model=Contact,
+            success_url=reverse_lazy("home"),
+            template_name='book/contact_delete_form.html'
+         ),
+         name='contact-delete')
 ]
--- book/views.py
+++ book/views.py
@@ -1,8 +1,5 @@
-from django.shortcuts import HttpResponse, render
-from django.urls import reverse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
 from . models import Contact
 
 # Create your views here.
@@ -23,12 +20,3 @@ class ContactView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
-    
-
-class CreateContact(CreateView):
-    
-    model = Contact
-    fields = '__all__'
-    
-    def get_success_url(self):
-        return reverse('home')
